01d29e7146a9dd452af2bf992311344f3e15300c
ebolo <daothanhduy305@gmail.com>
2018-01-03 22:49:42 +0700

    Add demo demonstrating getHtml function
    Signed-off-by: ebolo <daothanhduy305@gmail.com>

--- app/src/main/java/com/ebolo/kricheditor/MainActivity.kt
+++ app/src/main/java/com/ebolo/kricheditor/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.ebolo.krichtexteditor.fragments.KRichEditorFragment
 import com.ebolo.krichtexteditor.fragments.kRichEditorFragment
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.setContentView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editorFragment: KRichEditorFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MainActivityLayout().setContentView(this)
 
-        val editorFragment = kRichEditorFragment {
+        editorFragment = kRichEditorFragment {
             // This is just a dummy callback
             imageCallback = {
                 "https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/" +
@@ -38,7 +41,17 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_done -> {
+                editorFragment.editor.getHtml { html ->
+                    runOnUiThread {
+                        alert(
+                                message = html,
+                                title = "HTML"
+                        ).show()
+                    }
+                }
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
